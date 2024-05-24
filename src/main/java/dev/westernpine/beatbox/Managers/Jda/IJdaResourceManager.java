4c1ef4460e6e7a4907809371f8cbba6c66aaf639
WesternPine <tyler@westernpine.dev>
2024-05-24 13:56:21 -0400

    Patch Added in some functionality for handling audio connections and messages.

--- src/main/java/dev/westernpine/beatbox/Managers/Jda/IJdaResourceManager.java
+++ src/main/java/dev/westernpine/beatbox/Managers/Jda/IJdaResourceManager.java
@@ -1,5 +1,25 @@
 package dev.westernpine.beatbox.Managers.Jda;
 
+import net.dv8tion.jda.api.entities.Guild;
+import net.dv8tion.jda.api.entities.channel.concrete.TextChannel;
+import net.dv8tion.jda.api.entities.channel.middleman.AudioChannel;
+import net.dv8tion.jda.api.entities.channel.unions.AudioChannelUnion;
+import net.dv8tion.jda.api.interactions.components.ItemComponent;
+import net.dv8tion.jda.api.interactions.components.LayoutComponent;
+import net.dv8tion.jda.api.requests.restaction.MessageCreateAction;
+
+import java.util.Collection;
+import java.util.Optional;
+
 public interface IJdaResourceManager {
 
+    void connect(AudioChannel audioChannel);
+
+    void disconnect(Guild guild);
+
+    Optional<AudioChannelUnion> getAudioChannel(Guild guild);
+
+    MessageCreateAction sendMessage(TextChannel textChannel, Collection<? extends LayoutComponent> layoutComponents);
+
+    MessageCreateAction addActionRow(MessageCreateAction messageCreateAction, Collection<? extends ItemComponent> itemComponents);
 }
--- src/main/java/dev/westernpine/beatbox/Managers/Jda/Imp/JdaResourceManager.java
+++ src/main/java/dev/westernpine/beatbox/Managers/Jda/Imp/JdaResourceManager.java
@@ -10,17 +10,56 @@ import dev.westernpine.beatbox.Utilities.Configuration.Config.IConfig;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.entities.Activity;
+import net.dv8tion.jda.api.entities.Guild;
+import net.dv8tion.jda.api.entities.GuildVoiceState;
+import net.dv8tion.jda.api.entities.channel.concrete.TextChannel;
+import net.dv8tion.jda.api.entities.channel.middleman.AudioChannel;
+import net.dv8tion.jda.api.entities.channel.unions.AudioChannelUnion;
+import net.dv8tion.jda.api.interactions.components.ItemComponent;
+import net.dv8tion.jda.api.interactions.components.LayoutComponent;
+import net.dv8tion.jda.api.requests.restaction.MessageCreateAction;
+
+import java.util.Collection;
+import java.util.Optional;
 
 public class JdaResourceManager implements IJdaResourceManager {
+
+    private final JDA jda;
 
     @Inject
     public JdaResourceManager(IConfig config, ILavalinkManager lavalinkManager) {
         Configuration configuration = config.get();
         LavalinkClient client = lavalinkManager.get();
-        JDA jda = JDABuilder
+        jda = JDABuilder
                 .createDefault(configuration.requiredDiscordToken)
                 .setVoiceDispatchInterceptor(new JDAVoiceUpdateListener(client)) // See lavalink documentation for voice channels and guild audio manager.
                 .build();
         jda.getPresence().setActivity(Activity.customStatus(configuration.activity));
     }
+
+    @Override
+    public void connect(AudioChannel audioChannel) {
+        jda.getDirectAudioController().connect(audioChannel);
+    }
+
+    @Override
+    public void disconnect(Guild guild) {
+        jda.getDirectAudioController().disconnect(guild);
+    }
+
+    @Override
+    public Optional<AudioChannelUnion> getAudioChannel(Guild guild) {
+        return Optional.ofNullable(guild.getSelfMember().getVoiceState()).map(GuildVoiceState::getChannel);
+    }
+
+    @Override
+    public MessageCreateAction sendMessage(TextChannel textChannel, Collection<? extends LayoutComponent> layoutComponents) {
+        return textChannel.sendMessageComponents(layoutComponents);
+    }
+
+    @Override
+    public MessageCreateAction addActionRow(MessageCreateAction messageCreateAction, Collection<? extends ItemComponent> itemComponents) {
+        return messageCreateAction.addActionRow(itemComponents);
+    }
+
 }
